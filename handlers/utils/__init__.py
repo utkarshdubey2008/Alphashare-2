2a7edea88aa39daf0a1af62f5688b14abf448018
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2025-03-24 15:12:38 +0530

    Update __init__.py

--- handlers/utils/__init__.py
+++ handlers/utils/__init__.py
@@ -1,8 +1,13 @@
 from .message_delete import schedule_message_deletion
-from .utils import get_size_formatted, ButtonManager
+from .utils import (
+    get_size_formatted,
+    time_formatter,
+    ButtonManager
+)
 
 __all__ = [
     'schedule_message_deletion',
     'get_size_formatted',
+    'time_formatter',
     'ButtonManager'
 ]
